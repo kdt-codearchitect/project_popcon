200aee90f5384d99549a4cf95c2918909f2bf359
CarrotGold <chunius@naver.com>
2024-08-12 13:52:36 +0900

    fix/문의내역 조회 : QnaDTO 버그 수정

--- src/main/java/com/store/entity/CustomerQna.java
+++ src/main/java/com/store/entity/CustomerQna.java
@@ -53,13 +53,6 @@ public class CustomerQna {
 	
 
 //	타임스탬프는 SQL로 저장
-	@Column(name="qna_picture")
-	String qnaPicture;
-	
-	@Column(name="qna_image")
-	String qnaImage;
-	
-//	타임스탬프는 SQL로 저장
 	@Column(name="qna_date")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")	
 	LocalDateTime qnaDate;
