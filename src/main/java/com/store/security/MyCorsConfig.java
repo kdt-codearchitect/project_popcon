27a02aee46eb791d8b4e5d909041a188ba45bb33
system421 <system421@gmail.com>
2024-08-21 10:09:06 +0900

    feature : cors origin allow add

---
+++ src/main/java/com/store/security/MyCorsConfig.java
@@ -0,0 +1,27 @@
+package com.store.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyCorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://project-popcon-react.s3-website.ap-northeast-2.amazonaws.com",
+                            "https://projectpopcon.com",
+                            "http://localhost:3000"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
